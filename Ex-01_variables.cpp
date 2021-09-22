0770d18003afe020eb03de08c776efd45941c2c3
Djegdjiga Amazouz <59869785+Damazouz@users.noreply.github.com>
2021-09-22 20:49:47 +0200

    Update Ex-01_variables.cpp

--- Ex-01_variables.cpp
+++ Ex-01_variables.cpp
@@ -1,4 +1,19 @@
 # C++ syntax
 # Suiviez les instructions pour assigner et manipuler des variables
 
+# 1 er essai de script C++
 
+
+#include <iostream >
+#include <c s t d l i b >
+
+using namespace std ;
+
+i n t main ()
+{
+cout << " On apprends d'abord le C++ puis on écrira avec des programmes orientés objet " << endl ;
+r e t u r n 0;
+}
+
+
+# affiche des 
