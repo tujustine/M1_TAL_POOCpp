4b8fc1b9bb39db63dfcc1e0ec609aaeeced1a48c
Djegdjiga Amazouz <59869785+Damazouz@users.noreply.github.com>
2021-09-22 23:46:26 +0200

    Update Ex-01_variables.cpp

--- Ex-01_variables.cpp
+++ Ex-01_variables.cpp
@@ -3,17 +3,38 @@
 
 # 1 er essai de script C++
 
+#1-notions de bases : Écrire un programme qui affiche ceci à l'écran:
+"On apprends d'abord le C++ puis on écrira avec ce langage des programmes orientés objet"
 
-#include <iostream >
-#include <c s t d l i b >
+  
+******************************************************************
+
+#include <iostream> \\header file in c++ for input/output stream
+#include <cstdlib> C Standard General Utilities Library
 
 using namespace std ;
 
-i n t main ()
+int main ()
 {
 cout << " On apprends d'abord le C++ puis on écrira avec ce langage des programmes orientés objet " << endl ;
-r e t u r n 0;
+return 0;
 }
 
+******************************************************************
 
-# affiche des 
+#2-notions de bases : Écrire un programme qui affiche ceci à l'écran:
+"On apprends d'abord le C++ puis on écrira avec ce langage des programmes orientés objet"
+"On assignera une variable à la prochaine étape"
+
+
+
+
+#3-notions de bases: erire un programme qui affiche ceci à l'écran avec la variable "begin= 10" et "begin=12"
+"la réunion aura lieu de 10 à 12h"
+
+Indices= 
+  int begin=10;
+  int end=12;
+
+
+
